2a12335223a06fbd735b65a083114e7e808aa763
tefuna@gmail.com <tefuna@gmail.com>
2015-07-23 19:29:13 +0900

    commit

--- src/main/java/com/tefuna/sample/spring/controller/HelloController.java
+++ src/main/java/com/tefuna/sample/spring/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index() {
         return "hello/index";
     }
